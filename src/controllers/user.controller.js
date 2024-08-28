e7e181ef229431771bc651313517c523ca8d00c2
Barmantanmoy2211 <barmantanmoy59@gmail.com>
2024-08-28 15:02:36 +0530

    feat: Add getUserChannelProfile controller function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -336,6 +336,82 @@ const updateCoverImage = asyncHandler(async(req,res) => {
   
 })
 
+const getUserChannelProfile = asyncHandler(async(req,res) => {
+  const {username} = req.pasrams
+
+  if(!username?.trim()) {
+    throw new ApiError(400, "Username is required")
+  } 
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers"
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo"
+        },
+        isSubscribed: {
+          $cond: {
+            if: {
+              $in: [req.user?._id, "$subscribers.subscriber"]
+            },
+            then: true,
+            else: false
+            }
+          }
+        }
+      },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      }
+    }
+
+  ])
+
+  if(!channels?.length) {
+    throw new ApiError(404, "Channel does not exists")
+  }
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200, channel[0], "Channel details fetched successfully"))
+  
+})
+
+
+
 export {
   registerUser,
   loginUser,
@@ -345,5 +421,6 @@ export {
   getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
-  updateCoverImage
+  updateCoverImage,
+  getUserChannelProfile
 };
